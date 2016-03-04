2c038972b7c5c914562ec51e9176ed9322041bb1
James Gillard <jamesgillard@live.co.uk>
2016-03-04 23:52:10 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,149 @@
+var account_id;
+var access_token;
+
+$("#home-header, #cash-header").click(function() {
+  $("#home-page").toggle();
+  $("#cash-page").toggle();
+});
+
+$("#cash-form").submit(function() {
+  var loc = $("#cash-location").val();
+  var amount = parseFloat($("#cash-amount").val());
+  var desc = $("#cash-desc").val();
+  if (isNaN(amount)) {
+    alert("That's NaN");
+  } else {
+    var title = "£" + amount.toFixed(2) + " @ " + loc;
+    feedPost(title, desc);
+  }
+  return false;
+});
+
+function feedPost(title, body) {
+  var data = {
+    account_id: account_id,
+    type: "basic",
+    // url: ""
+    params: {
+      title: title,
+      image_url: "http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/cash-dark.png",
+      background_color: "#FFFFE5" 
+    }    
+  }
+  if (body) data.params.body = body;
+
+  $.ajax({
+    type: "POST",
+    url: "https://api.getmondo.co.uk/feed",
+    data: data,
+    dataType : "json",
+    beforeSend: function(xhr){xhr.setRequestHeader("Authorization", "Bearer " + access_token);},
+    success: function(data, textStatus, xhr){
+      if (xhr.status !== 200) alert(xhr.status, textStatus);
+      else {
+        $("#cash-submit").text("Done!");
+      }
+    },
+    failure: function(errMsg) { alert(errMsg); }
+  });
+}
+
+function queryMondo(url, data, callback) {
+  $.ajax({
+    url: url,
+    data: data,
+    dataType : "json",
+    beforeSend: function(xhr){xhr.setRequestHeader("Authorization", "Bearer " + access_token);},
+    success: function(data){
+      console.log(data);
+      callback(data);
+    },
+    failure: function(errMsg) {
+      alert(errMsg);
+    }
+  });
+}
+
+$("#account-button").click(function() {
+  queryMondo("https://api.getmondo.co.uk/accounts", null, function(data) {
+    account_id = data.accounts[0].id;
+    if (account_id) {
+      $("#home-header").fadeOut(600, function() { $(this).html("That's a nice name :)").fadeIn(); });
+      $("#account-button").text(data.accounts[0].description);
+      $("#balance-button").prop("disabled", false);
+      $("#cash-submit").prop("disabled", false);
+    } else {
+      alert("Something went wrong");
+    }
+  })
+});
+
+$("#balance-button").click(function() {
+  queryMondo("https://api.getmondo.co.uk/balance", {account_id: account_id}, function(data) {
+    if (data.balance) { 
+      var message = data.balance > 5000 ? "Life is good :D" : "Oh dear...";
+      $("#home-header").fadeOut(600, function() { $("#home-header").html(message).fadeIn(); }); 
+      $("#balance-button").text("£" + data.balance/100).fadeIn();
+      $("#transaction-button").prop("disabled", false);
+    }
+  });
+});
+
+$("#transaction-button").click(function() {
+  queryMondo("https://api.getmondo.co.uk/transactions", {account_id: account_id, "expand[]": "merchant"}, function(data) {
+    if (data.transactions) {
+      var latest = data.transactions[data.transactions.length - 1];
+      console.log(latest);
+      if (latest.is_load === true) latest = data.transactions[data.transactions.length - 2];
+      message = latest.merchant.name === "Tesco" ? "Yawn" : "What's that?";
+      $("#home-header").fadeOut(600, function() { $("#home-header").html(message).fadeIn(); });
+      $("#transaction-button").text(latest.merchant.name).fadeIn();
+    }
+  });
+});
+
+
+/*** ON WINDOW LOAD, GET ACCESS KEY ***/
+
+window.onload = function() {
+  var expected_state = "70tbc17xyt9732ytc4780t23479";
+  var params = getUrlVars();
+  if (params.state !== expected_state) {
+    alert("state does not match expected");
+  } else {
+    getAccessKey(params.code);
+  }        
+}
+
+function getUrlVars() {
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
+function getAccessKey(code) {
+  var data = {
+    code: code,
+    redirect_uri: "https://dl.dropboxusercontent.com/u/2666739/MondoCash/main.html"
+  }
+
+  $.ajax({
+    url: "https://5bvpwp95yb.execute-api.eu-west-1.amazonaws.com/prod/mondo-lambda-auth",
+    data: data,
+    success: function(data){
+      console.log(data);
+      access_token = data.access_token;
+      if (access_token) {
+        $("#account-button").prop('disabled', false);
+      } else {
+        alert("Something went wrong");
+      }
+    },
+    failure: function(errMsg) { alert(errMsg); }
+  });
+}
